d783687b023061341f84291b70b10ce5633311f0
Guilherme Henrique <guihcunha12@gmail.com>
2025-05-10 19:44:08 -0300

    agora vai

--- utils.py
+++ utils.py
@@ -7,10 +7,20 @@ def salvar_dados(caminho, dados):
         json.dump(dados, arquivo, indent=4, ensure_ascii=False)
 
 def carregar_dados(caminho):
-    if os.path.exists(caminho):
-        with open(caminho, 'r', encoding='utf-8') as arquivo:
-            return json.loas(arquivo)
-    return []
+    if not os.path.exists(caminho):
+        print(f"Arquivo '{caminho}' não encontrado. Criando um novo arquivo.")
+        return []
+
+    try:
+        with open(caminho, "r", encoding="utf-8") as arquivo:
+            if os.path.getsize(caminho) == 0:
+                print(f"Arquivo '{caminho}' está vazio. Retornando lista vazia.")
+                return []
+            return json.load(arquivo)
+    except json.JSONDecodeError:
+        print(f"Erro ao ler o arquivo '{caminho}'. O arquivo não está em formato JSON válido.")
+        return []
+
 
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')

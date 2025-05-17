2f2f0aed136632828a5c51ddb9fdcb7bd49859f7
Guilherme Henrique <guihcunha12@gmail.com>
2025-05-17 20:34:14 -0300

    login top

--- login.py
+++ login.py
@@ -2,6 +2,7 @@ import json
 import hashlib
 import os
 from utils import carregar_dados
+from cursos import exibir_menu2,selecionar_disciplina
 
 CAMINHO_ARQUIVO = 'data/alunos.json'
 
@@ -29,19 +30,22 @@ def autenticar_usuario():
 
     if usuario in usuarios and usuarios[usuario] == senha_hash:
         print("Login bem-sucedido!")
+        exibir_menu2()
+        while True:
+            try:
+                escolha2 = int(input("Selecione uma disciplina (0 para voltar): "))
+                if escolha2 == 0:
+                    print("Voltando ao menu principal...")
+                    break
+                disciplina = selecionar_disciplina(escolha2)
+                if disciplina:
+                    print(f"Você selecionou: {disciplina}")
+                else:
+                    print("Opção inválida. Tente novamente.")
+            except ValueError:
+                print("Entrada inválida. Digite um número.")
     else:
         print("Usuário ou senha incorretos.")
-
-def autenticar_usuario():
-    alunos = carregar_dados(CAMINHO_ARQUIVO)
-    nome = input("Usuário: ").strip().lower()
-    senha = input("Senha: ").strip()
-
-    if nome in alunos and alunos[nome] == gerar_hash(senha):
-        print(f"Login bem-sucedido. Bem-vindo, {nome}!")
-    else:
-        print("Usuário ou senha incorretos.")
-
 
 def lista_alunos():
     alunos = carregar_dados(CAMINHO_ARQUIVO)
@@ -50,3 +54,13 @@ def lista_alunos():
         return
     for idx, aluno in enumerate(alunos, start=1):
         print(f"{idx}. {aluno['nome']} | Idade: {aluno['idade']} |senha: {aluno['senha']}")
+
+# def autenticar_usuario():
+#     alunos = carregar_dados(CAMINHO_ARQUIVO)
+#     nome = input("Usuário: ").strip().lower()
+#     senha = input("Senha: ").strip()
+
+#     if nome in alunos and alunos[nome] == gerar_hash(senha):
+#         print(f"Login bem-sucedido. Bem-vindo, {nome}!")
+#     else:
+#         print("Usuário ou senha incorretos.")
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Arquivo principal que executa o menu do sistema
 from alunos import cadastrar_alunos
 from login import autenticar_usuario
-from cursos import exibir_menu2, selecionar_disciplina
 
 def exibir_menu():
     while True:
@@ -13,25 +12,9 @@ def exibir_menu():
         escolha = input("Escolha uma opção: ")
 
         if escolha == "1":
-            if autenticar_usuario():
-                while True:
-                    exibir_menu2()
-                    try:
-                        escolha2 = int(input("Selecione uma disciplina (0 para voltar): "))
-                        if escolha2 == 0:
-                            print("Voltando ao menu principal...")
-                            break
-                        disciplina = selecionar_disciplina(escolha2)
-                        if disciplina:
-                            print(f"Você selecionou: {disciplina}")
-                        else:
-                            print("Opção inválida. Tente novamente.")
-                    except ValueError:
-                        print("Entrada inválida. Digite um número.")
-        
+            autenticar_usuario()
         elif escolha == "2":
             cadastrar_alunos()
-            
         elif escolha == "0":
             print("Encerrando o sistema.")
             break

cd8918caabaaf8f62bd37b3eda4f9e2519141420
guilhermecunha777 <guihcunha12@gmail.com>
2025-05-21 23:57:04 -0300

    busca cursos por nome

--- cursos.py
+++ cursos.py
@@ -1,6 +1,8 @@
 # Módulo com cursos e disciplinas
 from utils import add_cursos,listar_cursos
 
+caminho = 'data/cursos.json'
+
 def exibir_menu2():
     print("\n=== MENU DE CURSOS E DISCIPLINAS ===")
     print("1. Tecnologia da Informação e Comunicação")
@@ -25,6 +27,25 @@ def selecionar_disciplina(opcao):
         8: "Cibersegurança"
     }
     return disciplinas.get(opcao, None)
+
+def buscar_curso_por_nome():
+    try:
+        with open(caminho, 'r', encoding='utf-8') as f:
+            cursos= json.load(f)
+    except FileNotFoundError:
+        print("erro ao decodificar o arquivo JSON.")
+        return
+    
+    nome_buscar = input("Digite o nome (ou parte do nome) do curso que deseja buscar: ").strip().lower()
+
+    encontrados = [curso for curso in cursos if nome_buscar in curso["nome"].lower()]
+
+    if encontrados:
+        print("\nCursos encontrados: ")
+        for curso in encontrados:
+            print(f"ID:{curso['id']}, nome: {curso['nome']}, conteudo: {curso['conteudo']}")
+    else:
+        print("nenhum curso encontrado com esse nome.")
 
 def menu_prof():
     print('1. adicionar um cursos')
